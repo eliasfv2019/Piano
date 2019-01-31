5deb05635e58910f5125a3891dd89096422a26ad
eliasfv2019 <32773105+eliasfv2019@users.noreply.github.com>
2019-01-31 13:43:50 -0400

    Add files via upload

--- Piano.java
+++ Piano.java
@@ -1,5 +1,4 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
-
 /**
  * A piano that can be played with the computer keyboard.
  * 
@@ -8,6 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+    private String[] blackKeys = {"w", "e", "","t","y","u","","o","p"};
     private String[] whiteKeys = {"a","s","d","f","g","h","j","k","l",";","'","\\"};
     private String[] whiteNotes = {"3c","3d","3e","3f","3g","3a","3b","4c","4d", "4e","4f","4g"};
     /**
@@ -17,9 +17,17 @@ public class Piano extends World
     {
         super(800, 340, 1);
         int i = 0;
-        while(i<12){
+        int q = 0;
+        while(i < 12){
             addObject(new Key("white", whiteKeys[i], whiteNotes[i]+".wav"),i*63+55,140);
             i++;
         }
-    }
+        while(q < 9){
+            addObject(new Key("black", blackKeys[q], whiteNotes[q]+".wav"),q*63+55,140);
+            q++;
+        }
+        while(blackKeys[q].equals("")){
+            
+        }
+   }
 }
